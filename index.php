632e121a4b3676b8813eec201f82bb3de8838938
saiba2015 <saibahasan15@gmail.comm>
2024-09-03 17:52:59 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class-04</title>
+</head>
+<body>
+    <h1>Hello PHP</h1> 
+
+    <h1><?php echo "Hello"; ?> PHP</h1> 
+
+    <?php
+    
+    $name = "sumon";
+
+    $price = 100;
+
+    $address = "Test area, Test District, 758478";
+
+    $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel deserunt omnis deleniti animi dolorem quod recusandae? Obcaecati ab ullam voluptatum ipsam, animi facilis sunt sequi corporis repellat unde deserunt quam.";
+    
+    //echo $name;
+
+    $my_school = "";
+
+    $mySchool = "";
+
+    $mySchoolName = "";
+
+    $MySchoolName = "";
+
+    $first_name = "Sumon";
+
+    $total_amount = 10;
+
+    echo "Hello $first_name, Your Total Amount : $total_amount"; 
+    
+
+    ?>
+
+
+
+</body>
+</html>
